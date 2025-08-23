f26109495ebd563c88e98ffeafc7a1007f4aef7a
darmuh <darmuh8@gmail.com>
2025-08-23 13:25:00 -0700

    namespace needs brackets or else unity throws below error:
    "error CS8773: Feature 'file-scoped namespace' is not available in C# 9.0. Please use language version 10.0 or greater."

--- Scripts/MoreEyesMod.cs
+++ Scripts/MoreEyesMod.cs
@@ -3,71 +3,73 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-namespace MoreEyes.SDK;
-/// <summary>
-/// A MoreEyes content mod.
-/// This should only be used internally by this mod or by editor scripts within unity
-/// </summary>
-[CreateAssetMenu(menuName = "MoreEyes/Mod", order = 0, fileName = "New MoreEyes Mod")]
-public class MoreEyesMod : ScriptableObject
+namespace MoreEyes.SDK
 {
-    [SerializeField]
-    private string _name = null!;
-
-    [SerializeField]
-    private string _author = null!;
-
-    [SerializeField]
-    private string _version = "1.0.0";
-
     /// <summary>
-    /// The name of this mod.
+    /// A MoreEyes content mod.
+    /// This should only be used internally by this mod or by editor scripts within unity
     /// </summary>
-    public string Name => _name;
+    [CreateAssetMenu(menuName = "MoreEyes/Mod", order = 0, fileName = "New MoreEyes Mod")]
+    public class MoreEyesMod : ScriptableObject
+    {
+        [SerializeField]
+        private string _name = null!;
 
-    /// <summary>
-    /// The author of this mod.
-    /// </summary>
-    public string Author => _author;
+        [SerializeField]
+        private string _author = null!;
 
-    /// <summary>
-    /// The version of this mod.
-    /// </summary>
-    public string Version => _version;
+        [SerializeField]
+        private string _version = "1.0.0";
 
-    /// <summary>
-    /// Unqique identifier of this mod.<br/>
-    /// Format is $"{<see cref="Author"/>}-{<see cref="Name"/>}-{<see cref="Version"/>}".
-    /// </summary>
-    public string Identifier => $"{Author}-{Name}-{Version}";
+        /// <summary>
+        /// The name of this mod.
+        /// </summary>
+        public string Name => _name;
 
-    [SerializeField]
-    private List<GameObject> _prefabs = new();
+        /// <summary>
+        /// The author of this mod.
+        /// </summary>
+        public string Author => _author;
 
-    /// <summary>
-    /// This mods' specific prefabs for custom iris/pupils
-    /// </summary>
-    public List<GameObject> Prefabs => _prefabs;
+        /// <summary>
+        /// The version of this mod.
+        /// </summary>
+        public string Version => _version;
+
+        /// <summary>
+        /// Unqique identifier of this mod.<br/>
+        /// Format is $"{<see cref="Author"/>}-{<see cref="Name"/>}-{<see cref="Version"/>}".
+        /// </summary>
+        public string Identifier => $"{Author}-{Name}-{Version}";
+
+        [SerializeField]
+        private List<GameObject> _prefabs = new();
+
+        /// <summary>
+        /// This mods' specific prefabs for custom iris/pupils
+        /// </summary>
+        public List<GameObject> Prefabs => _prefabs;
 
 
-    /// <summary>
-    /// Used to set name of mod, public for editor script ONLY
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetName(string value) => _name = value;
-    /// <summary>
-    /// Used to set author of mod, public for editor script ONLY
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetAuthor(string value) => _author = value;
-    /// <summary>
-    /// Used to set version of mod, public for editor script ONLY
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetVersion(string value) => _version = value;
-    /// <summary>
-    /// Used to set prefabs of mod, public for editor script ONLY
-    /// </summary>
-    /// <param name="value"></param>
-    public void SetPrefabs(List<GameObject> value) => _prefabs = value;
+        /// <summary>
+        /// Used to set name of mod, public for editor script ONLY
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetName(string value) => _name = value;
+        /// <summary>
+        /// Used to set author of mod, public for editor script ONLY
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetAuthor(string value) => _author = value;
+        /// <summary>
+        /// Used to set version of mod, public for editor script ONLY
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetVersion(string value) => _version = value;
+        /// <summary>
+        /// Used to set prefabs of mod, public for editor script ONLY
+        /// </summary>
+        /// <param name="value"></param>
+        public void SetPrefabs(List<GameObject> value) => _prefabs = value;
+    }
 }
