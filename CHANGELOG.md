feaa4422111b3474f43879f010b5d6b0ea8853c4
darmuh <darmuh8@gmail.com>
2025-08-27 00:17:30 -0700

    visual overhaul of editorgui menus bumping version number due to number of changes

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,4 +1,9 @@
 # MoreEyesSDK Changelog
 
+### Version 1.0.1
+- Visuals overhaul for all MoreEyes Menus.
+- Clicking ``Package Mods`` will now prompt you with a list of detected MoreEyesMods ScriptableObjects and their associated prefabs.
+- In the ``Package Mods`` menu you can now select which bundles you'd like to build, all are enabled by default.
+
 ### Version 1.0.0
 - Initial version created by darmuh
--- Editor/MoreEyesEditorMenu.cs
+++ Editor/MoreEyesEditorMenu.cs
@@ -9,40 +9,160 @@ using UnityEngine;
 using UnityEngine.UIElements;
 public class MoreEyesEditorMenu : EditorWindow
 {
+    internal enum SelectedMenuItem
+    {
+        CreateMod,
+        PackageMods
+    }
+    internal static SelectedMenuItem CurrentWindow;
+
     //Create MoreEyes Mod
     [MenuItem("More Eyes/Create Mod")]
     static void CreateModAsset()
     {
-        var window = GetWindow<MoreEyesEditorMenu>();
-        window.titleContent = new GUIContent("Create MoreEyes Mod");
+        CurrentWindow = SelectedMenuItem.CreateMod;
+        if (HasOpenInstances<MoreEyesEditorMenu>())
+            GetWindow<MoreEyesEditorMenu>().Close();
+
+        GetWindow<MoreEyesEditorMenu>("Create MoreEyes Mod");
     }
 
     private static TextField modName;
     private static TextField modVer;
     private static TextField modAuthor;
 
+    private static VisualElement HorizontalLine()
+    {
+        VisualElement horizontalLine = new();
+        horizontalLine.style.height = 1;
+        horizontalLine.style.backgroundColor = new Color(0.5f, 0.5f, 0.5f, 0.3f);
+        horizontalLine.style.marginTop = 10;
+        horizontalLine.style.marginBottom = 10;
+
+        return horizontalLine;
+    }
 
     public void CreateGUI()
     {
         // Each editor window contains a root VisualElement object
         VisualElement root = rootVisualElement;
-
-        // VisualElements objects can contain other VisualElement following a tree hierarchy
-        Label textlabel = new("Create a Mod Asset for your More Eyes Mod.\n");
-        modName = new("Mod Name", 32, false, false, '*');
-        root.Add(modName);
-        modVer = new("Mod Version", 5, false, false, '*');
-        root.Add(modVer);
-        modAuthor = new("Mod Author", 64, false, false, '*');
-        root.Add(modAuthor);
-
-        // Create button
-        Button submit = new(CreateMod)
+        ScrollView scrollView = new()
         {
-            name = "Submit",
-            text = "Create Mod"
+            horizontalScrollerVisibility = ScrollerVisibility.Hidden
         };
-        root.Add(submit);
+        rootVisualElement.Add(scrollView);
+
+        if (CurrentWindow == SelectedMenuItem.CreateMod)
+        {
+            Label header = new($"Build your Mod's Scriptable Object");
+            header.style.fontSize = 16;
+            header.style.unityTextAlign = TextAnchor.MiddleCenter;
+            header.style.unityFontStyleAndWeight = FontStyle.Bold;
+            header.style.paddingBottom = 3;
+            header.style.paddingLeft = 1;
+            header.style.paddingRight = 1;
+            header.style.paddingTop = 14;
+            root.Add(header);
+            root.Add(HorizontalLine());
+            modName = new("Mod Name", 32, false, false, '*');
+            root.Add(modName);
+            modVer = new("Mod Version", 5, false, false, '*');
+            root.Add(modVer);
+            modAuthor = new("Mod Author", 64, false, false, '*');
+            root.Add(modAuthor);
+            root.Add(HorizontalLine());
+            // Create button
+            Button submit = new(CreateMod)
+            {
+                name = "Submit",
+                text = "Create Mod"
+            };
+            root.Add(submit);
+
+            return;
+        }
+
+        if (CurrentWindow == SelectedMenuItem.PackageMods)
+        {
+            var guids = AssetDatabase.FindAssets("t:MoreEyesMod");
+            List<MoreEyesMod> mods = new();
+            foreach (var guid in guids)
+            {
+                string assetPath = AssetDatabase.GUIDToAssetPath(guid);
+                mods.Add(AssetDatabase.LoadAssetAtPath(assetPath, typeof(MoreEyesMod)) as MoreEyesMod);
+            }
+
+            if (mods.Count == 0)
+            {
+                Debug.LogWarning("No existing Mod assets found!");
+                return;
+            }
+
+            List<ModTrackValues> modTracker = new();
+
+            foreach (var mod in mods)
+            {
+                Label header = new($"{mod.Name}");
+                header.style.fontSize = 16;
+                header.style.unityTextAlign = TextAnchor.MiddleCenter;
+                header.style.unityFontStyleAndWeight = FontStyle.Bold;
+                header.style.paddingBottom = 3;
+                header.style.paddingLeft = 1;
+                header.style.paddingRight = 1;
+                header.style.paddingTop = 3;
+                root.Add(header);
+                Foldout foldout = new()
+                {
+                    value = false
+                };
+                root.Add(foldout);
+                TextField path = new("Build path", 999, false, false, '*')
+                {
+                    tooltip = "Click to set path...",
+                    value = ""
+                };
+                path.RegisterCallback<ClickEvent>(click =>
+                {
+                    string newPath = EditorUtility.SaveFolderPanel($"{mod.name} Build Location", "", "");
+                    if (!string.IsNullOrEmpty(newPath))
+                        path.value = newPath;
+
+                    Debug.Log($"Selected path {newPath} to build {mod.name} bundle");
+                });
+                root.Add(path);
+                Toggle modToggle = new()
+                {
+                    value = true,
+                    text = "Package Contents",
+                };
+                root.Add(modToggle);
+                modTracker.Add(new ModTrackValues(mod, path, modToggle));
+
+                foreach (var item in mod.Prefabs)
+                {
+                    foldout.Add(new Label($"{item.name}"));
+                    foldout.text = $"{mod.Name} Prefab List [{mod.Prefabs.Count}]";
+                }
+
+                root.Add(HorizontalLine());
+            }
+
+            Button submit = new(() =>
+            {
+                List<ModTrackValues> buildList = new();
+                foreach (var item in modTracker)
+                {
+                    if (item.BuildBundle.value && !string.IsNullOrEmpty(item.Path.text))
+                        buildList.Add(item);
+                }
+                BundleMods(buildList);
+            })
+            {
+                name = "Submit",
+                text = "Create Bundles"
+            };
+            root.Add(submit);
+        }
     }
 
     private void CreateMod()
@@ -61,71 +181,62 @@ public class MoreEyesEditorMenu : EditorWindow
         window.Close();
     }
 
-    [MenuItem("More Eyes/Package Mods")]
-    private static void BuildAssetBundle()
+    private void BundleMods(List<ModTrackValues> buildList)
     {
-        var guids = AssetDatabase.FindAssets("t:MoreEyesMod");
-        List<MoreEyesMod> mods = new();
-        foreach (var guid in guids)
-        {
-            string assetPath = AssetDatabase.GUIDToAssetPath(guid);
-            mods.Add(AssetDatabase.LoadAssetAtPath(assetPath, typeof(MoreEyesMod)) as MoreEyesMod);
-        };
-
-        if (mods.Count == 0)
-        {
-            Debug.LogWarning("No existing Mod assets found!");
-            return;
-        }
-
-        string getPath = EditorUtility.SaveFolderPanel($"EyesBundles Location", "", "");
-        if (string.IsNullOrEmpty(getPath))
-            return;
-
-        List<AssetBundleBuild> bundlebuilds = new();
         string bundles = "Assets/bundleFiles";
         if (!AssetDatabase.IsValidFolder(bundles))
             AssetDatabase.CreateFolder("Assets", "bundleFiles");
 
-
-        foreach (var mod in mods)
+        foreach (var item in buildList)
         {
+            List<AssetBundleBuild> bundlebuilds = new();
             List<string> assets = new()
             {
-                AssetDatabase.GetAssetPath(mod)
+                AssetDatabase.GetAssetPath(item.Mod)
             };
-            foreach (var prefab in mod.Prefabs)
+            foreach (var prefab in item.Mod.Prefabs)
                 assets.Add(AssetDatabase.GetAssetPath(prefab));
 
             foreach (string name in assets)
-                Debug.Log($"bundling {name} in mod: {mod.Name}");
+                Debug.Log($"bundling {name} in item: {item.Mod.Name}");
 
             bundlebuilds.Add(new AssetBundleBuild()
             {
-                assetBundleName = mod.name,
+                assetBundleName = item.Mod.name,
                 assetNames = assets.ToArray(),
             });
+
+            var buildParams = new BuildAssetBundlesParameters()
+            {
+                bundleDefinitions = bundlebuilds.ToArray(),
+                outputPath = bundles,
+                options = BuildAssetBundleOptions.ChunkBasedCompression,
+                targetPlatform = BuildTarget.StandaloneWindows64
+            };
+
+            var file = BuildPipeline.BuildAssetBundles(buildParams);
+
+            foreach (var fileName in file.GetAllAssetBundles())
+            {
+                string finalBundlePath = Path.Combine(item.Path.text, fileName + ".eyes");
+                if (File.Exists(finalBundlePath))
+                    File.Delete(finalBundlePath);
+
+                File.Copy(Path.Combine(bundles, fileName), finalBundlePath);
+                Debug.Log($"Eyes Bundle saved to {Path.GetFullPath(finalBundlePath)}");
+            }
         }
+    }
 
-        var buildParams = new BuildAssetBundlesParameters()
-        {
-            bundleDefinitions = bundlebuilds.ToArray(),
-            outputPath = bundles,
-            options = BuildAssetBundleOptions.ChunkBasedCompression,
-            targetPlatform = BuildTarget.StandaloneWindows64
-        };
 
-        var file = BuildPipeline.BuildAssetBundles(buildParams);
+    [MenuItem("More Eyes/Package Mods")]
+    private static void BuildAssetBundle()
+    {
+        CurrentWindow = SelectedMenuItem.PackageMods;
+        if (HasOpenInstances<MoreEyesEditorMenu>())
+            GetWindow<MoreEyesEditorMenu>().Close();
 
-        foreach (var fileName in file.GetAllAssetBundles())
-        {
-            string finalBundlePath = Path.Combine(getPath, fileName + ".eyes");
-            if (File.Exists(finalBundlePath))
-                File.Delete(finalBundlePath);
-
-            File.Copy(Path.Combine(bundles, fileName), finalBundlePath);
-            Debug.Log($"Eyes Bundle saved to {Path.GetFullPath(finalBundlePath)}");
-        }
+        GetWindow<MoreEyesEditorMenu>("Package MoreEyes Mod");
     }
 
     //https://discussions.unity.com/t/how-to-get-path-from-the-current-opened-folder-in-the-project-window-in-unity-editor/226209
@@ -135,5 +246,20 @@ public class MoreEyesEditorMenu : EditorWindow
         MethodInfo getActiveFolderPath = projectWindowUtilType.GetMethod("GetActiveFolderPath", BindingFlags.Static | BindingFlags.NonPublic);
         object obj = getActiveFolderPath.Invoke(null, new object[0]);
         return obj.ToString();
+    }
+
+    internal class ModTrackValues
+    {
+        internal MoreEyesMod Mod;
+        internal TextField Path;
+        internal Toggle BuildBundle;
+        internal Toggle BuildManifest;
+
+        internal ModTrackValues(MoreEyesMod _mod, TextField _path, Toggle _buildBundle)
+        {
+            Mod = _mod;
+            Path = _path;
+            BuildBundle = _buildBundle;
+        }
     }
 }
